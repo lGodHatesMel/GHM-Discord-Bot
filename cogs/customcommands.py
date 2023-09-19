ed98be531e9ad4ff7ebffab7929053425961ed1e
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-09-19 00:19:44 -0400

    Make custom commands useable

--- cogs/customcommands.py
+++ cogs/customcommands.py
@@ -3,20 +3,37 @@ from discord.ext import commands
 import json
 import os
 
-#script_directory = os.path.dirname('GHM-Discord-Bot')
-#custom_commands_file = os.path.join(script_directory, 'Database', 'custom_commands.json')
-
 class CustomCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.database_folder = 'Database'
         self.commands_file = os.path.join(self.database_folder, 'custom_commands.json')
-        #self.commands_file = custom_commands_file
 
         # Check if the custom_commands.json file exists, and create it if not
         if not os.path.exists(self.commands_file):
             with open(self.commands_file, 'w') as file:
                 json.dump({}, file)
+
+        # Load custom commands when the bot starts
+        self.load_custom_commands()
+
+    def load_custom_commands(self):
+        try:
+            # Load existing custom commands from JSON
+            with open(self.commands_file, 'r') as file:
+                custom_commands = json.load(file)
+
+            # Register each custom command with the bot
+            for command_name, command_response in custom_commands.items():
+                # Create a new custom command function
+                async def custom_command(ctx):
+                    await ctx.send(command_response)
+
+                # Register the custom command with the bot
+                self.bot.add_command(commands.Command(custom_command, name=command_name))
+
+        except Exception as e:
+            print(f'An error occurred while loading custom commands: {str(e)}')
 
     @commands.command(name='addcommand')
     async def add_custom_command(self, ctx, command_name, *, command_response):
@@ -24,6 +41,9 @@ class CustomCommands(commands.Cog):
             # Load existing custom commands from JSON
             with open(self.commands_file, 'r') as file:
                 custom_commands = json.load(file)
+
+            # Convert the command_name to lowercase
+            command_name = command_name.lower()
 
             # Check if the command name already exists
             if command_name in custom_commands:
@@ -37,6 +57,11 @@ class CustomCommands(commands.Cog):
             with open(self.commands_file, 'w') as file:
                 json.dump(custom_commands, file, indent=4)
 
+            # Register the new custom command with the bot
+            async def custom_command(ctx):
+                await ctx.send(command_response)
+            self.bot.add_command(commands.Command(custom_command, name=command_name))
+
             await ctx.send(f'Command "{command_name}" added successfully.')
         except Exception as e:
             await ctx.send(f'An error occurred: {str(e)}')
@@ -47,6 +72,9 @@ class CustomCommands(commands.Cog):
             # Load existing custom commands from JSON
             with open(self.commands_file, 'r') as file:
                 custom_commands = json.load(file)
+
+            # Convert the command_name to lowercase
+            command_name = command_name.lower()
 
             # Check if the command name exists
             if command_name not in custom_commands:
@@ -60,6 +88,11 @@ class CustomCommands(commands.Cog):
             with open(self.commands_file, 'w') as file:
                 json.dump(custom_commands, file, indent=4)
 
+            # Update the custom command function
+            async def custom_command(ctx):
+                await ctx.send(new_response)
+            self.bot.add_command(commands.Command(custom_command, name=command_name))
+
             await ctx.send(f'Command "{command_name}" updated successfully.')
         except Exception as e:
             await ctx.send(f'An error occurred: {str(e)}')
@@ -70,6 +103,9 @@ class CustomCommands(commands.Cog):
             # Load existing custom commands from JSON
             with open(self.commands_file, 'r') as file:
                 custom_commands = json.load(file)
+
+            # Convert the command_name to lowercase
+            command_name = command_name.lower()
 
             # Check if the command name exists
             if command_name not in custom_commands:
@@ -82,6 +118,9 @@ class CustomCommands(commands.Cog):
             # Save the updated custom commands to JSON
             with open(self.commands_file, 'w') as file:
                 json.dump(custom_commands, file, indent=4)
+
+            # Unregister the custom command from the bot
+            self.bot.remove_command(command_name)
 
             await ctx.send(f'Command "{command_name}" deleted successfully.')
         except Exception as e:
