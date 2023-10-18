9ccfdd555895dd392488ded360e72f2a98153b5c
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2023-10-18 13:40:06 -0400

    Twitch API stuff?
    Note: Find someone that can help explain a bit more on using twitch API info and channel reward stuff.

--- cogs/twitchrewards.py
+++ cogs/twitchrewards.py
@@ -10,9 +10,28 @@ class TwitchRewards(commands.Cog):
         # add actions
         await ctx.send('Command was triggered! ??')
 
-    def listen_to_twitch(self):
-        # Add the code for registering with Twitch here
-        pass
+    def listen_to_twitch():
+        url = 'https://api.twitch.tv/helix/eventsub/subscriptions'
+        headers = {
+            'Client-ID': 'TWITCH_CLIENT_ID',
+            'Authorization': f'Bearer {TWITCH_API_TOKEN}',
+        }
+        data = {
+            "type": "channel.channel_points_custom_reward_redemption.add",
+            "version": "1",
+            "condition": {
+                "broadcaster_user_id": "TWITCH_USER_ID",
+            },
+            "transport": {
+                "method": "webhook",
+                "callback": "WEBHOOK_URL",
+                "secret": "WEBHOOK_SECRET",
+            }
+        }
+
+        response = requests.post(url, json=data, headers=headers)
+        response_data = response.json()
+        return response_data
 
     @commands.Cog.listener()
     async def on_ready(self):
