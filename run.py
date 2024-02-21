8fcf6ba53dc3cf9dbd8a6488b09378363b14252f
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2024-02-21 00:16:56 -0500

    update run.py

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
+import json
+import requests
+from io import BytesIO
 import tkinter as tk
 from PIL import Image, ImageTk
 import subprocess
+import os
 
-# Function to start the Python script
+with open('config.json') as f:
+    config = json.load(f)
+
 def discord_start_script():
     global script_process
     print('Starting Discord Bot')
-    script_process = subprocess.Popen(["python", "main.py"])
+    script_process = subprocess.Popen([os.path.join(os.sys.executable, "main.py")])
 
-# Function to stop the Python script
 def discord_stop_script():
     global script_process
     if script_process:
@@ -16,32 +21,41 @@ def discord_stop_script():
         script_process.terminate()
         script_process = None
 
-# Create the main window
 window = tk.Tk()
 window.title("GHM Discord Bot")
+window.geometry("300x200")
+window.configure(bg="#222222")
+response = requests.get(config['logo_url'])
+logo_image = Image.open(BytesIO(response.content))
 
-# Set initial size of the window
-window.geometry("100x100")  # You can adjust the width and height as needed
+global logo_photo
+logo_photo = ImageTk.PhotoImage(logo_image)
 
-# Configure background color
-window.configure(bg="#FF0000")
+logo_label = tk.Label(window, image=logo_photo, bg="#222222")
+logo_label.image = logo_photo
+logo_label.place(relx=0.5, rely=0.3, anchor='center')
 
-# # Add a logo/image
-logo_image = Image.open(r"C:\GHM-Discord-Bot\images\Logo.png")  # Use the 'r' prefix to treat the string as a raw string
-#logo_photo = ImageTk.PhotoImage(logo_image)
-#logo_label = tk.Label(window, image=logo_photo, bg="#FF0000")  # Adjust bg color to match the window
-#logo_label.pack()
+## NEED TO FIGURE OUT HOW TO MAKE THE IMAGE RESIZE PROPERLY WITH OUT SPAMMING THE CONSOLE
+## `Vnew_photo = ImageTk.PhotoImage(width=width, height=height)` SEEMS WITH OUT THIS THE LOGO WONT SHOW
+def resize_logo(event):
+    global logo_photo, logo_image
+    width = event.width // 3
+    height = event.height // 3
+    new_photo = ImageTk.PhotoImage(width=width, height=height)
+    resized_image = logo_image.resize((width, height), Image.Resampling.LANCZOS)
+    new_photo.paste(resized_image)
+    logo_photo = new_photo
+    logo_label.config(image=logo_photo)
 
-# Create "Start" button
-start_button = tk.Button(window, text="Discord Start", command=discord_start_script, bg="#7289DA", fg="white")
-start_button.pack()
+window.bind('<Configure>', resize_logo)
 
-# Create "Stop" button
-stop_button = tk.Button(window, text="Discord Stop", command=discord_stop_script, bg="#7289DA", fg="white")
-stop_button.pack()
+custom_font = ("Helvetica", 14)
+start_button = tk.Button(window, text="Discord Start", command=discord_start_script, bg="#7289DA", fg="white", font=custom_font)
+start_button.place(relx=0.25, rely=0.7, anchor='center', width=120, height=30)
 
-# Initialize script_process as None
+stop_button = tk.Button(window, text="Discord Stop", command=discord_stop_script, bg="#7289DA", fg="white", font=custom_font)
+stop_button.place(relx=0.75, rely=0.7, anchor='center', width=120, height=30)
+
 script_process = None
 
-# Run the GUI
 window.mainloop()
