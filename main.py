bb3749efdb7cfe3da7058c2a2bbc548a92a211ed
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2024-03-10 18:56:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,50 +9,53 @@ import utils.utils as utils
 from utils.Paginator import Paginator
 import os
 import traceback
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
+# class EmbedHelpCommand(commands.HelpCommand):
+#     def __init__(self):
+#         super().__init__()
+#     def get_command_signature(self, command):
+#         if command.help:
+#             return '`!{} {}`'.format(command.qualified_name, command.help)
+#         else:
+#             return '`!{}`'.format(command.qualified_name)
 
-class EmbedHelpCommand(commands.HelpCommand):
-    def __init__(self):
-        super().__init__()
-    def get_command_signature(self, command):
-        if command.help:
-            return '`!{} {}`'.format(command.qualified_name, command.help)
-        else:
-            return '`!{}`'.format(command.qualified_name)
-
-    async def send_bot_help(self, mapping):
-        ExcludeCommands = ['staffcommands', 'commands', 'help', 'ping', 'botping']
-        embeds = []
-        current_embed = discord.Embed(color=discord.Color.random())
-        current_count = 0
-        for cog, commands in mapping.items():
-            if getattr(cog, "hidden", False):
-                continue
-            commands = [c for c in commands if not c.hidden and c.name not in ExcludeCommands]
-            if commands:
-                cog_name = getattr(cog, "qualified_name", "Other Commands")
-                field_value = ""
-                for command in commands:
-                    signature = self.get_command_signature(command)
-                    field_value += signature + "\n"
-                    current_count += 1
-                    if current_count == 9:
-                        current_embed.add_field(name=f"{cog_name}", value=field_value, inline=True)
-                        current_embed.title = f"**Server Commands - Page {len(embeds) + 1}**"
-                        embeds.append(current_embed)
-                        current_embed = discord.Embed(color=discord.Color.random())
-                        current_count = 0
-                        field_value = ""
-                if field_value:
-                    current_embed.add_field(name=f"{cog_name}", value=field_value, inline=True)
-                current_embed.set_footer(text="Use the reactions to navigate between pages.")
-        if len(current_embed.fields) > 0:
-            current_embed.set_footer(text="Use the reactions to navigate between pages.")
-            current_embed.title = f"**Server Commands - Page {len(embeds) + 1}**"
-            embeds.append(current_embed)
-        paginator = Paginator(self.context, embeds)
-        await paginator.start()
+#     async def send_bot_help(self, mapping):
+#         ExcludeCommands = ['staffcommands', 'commands', 'help', 'ping', 'botping']
+#         embeds = []
+#         current_embed = discord.Embed(color=discord.Color.random())
+#         current_count = 0
+#         for cog, commands in mapping.items():
+#             if getattr(cog, "hidden", False):
+#                 continue
+#             commands = [c for c in commands if not c.hidden and c.name not in ExcludeCommands]
+#             if commands:
+#                 cog_name = getattr(cog, "qualified_name", "Other Commands")
+#                 field_value = ""
+#                 for command in commands:
+#                     signature = self.get_command_signature(command)
+#                     field_value += signature + "\n"
+#                     current_count += 1
+#                     if current_count == 9:
+#                         current_embed.add_field(name=f"{cog_name}", value=field_value, inline=True)
+#                         current_embed.title = f"**Server Commands - Page {len(embeds) + 1}**"
+#                         embeds.append(current_embed)
+#                         current_embed = discord.Embed(color=discord.Color.random())
+#                         current_count = 0
+#                         field_value = ""
+#                 if field_value:
+#                     current_embed.add_field(name=f"{cog_name}", value=field_value, inline=True)
+#                 current_embed.set_footer(text="Use the reactions to navigate between pages.")
+#         if len(current_embed.fields) > 0:
+#             current_embed.set_footer(text="Use the reactions to navigate between pages.")
+#             current_embed.title = f"**Server Commands - Page {len(embeds) + 1}**"
+#             embeds.append(current_embed)
+#         paginator = Paginator(self.context, embeds)
+#         await paginator.start()
 
 ## Bot Setup
 bot = commands.Bot(
@@ -60,18 +63,18 @@ bot = commands.Bot(
     case_insensitive=True,
     intents = discord.Intents.all(),
     owner_ids=[ROLEIDS["OWNERID"]],
-    help_command=EmbedHelpCommand(),
+    # help_command=EmbedHelpCommand(),
     description="Custom bot for our Discord server."
 )
 slash = SlashCommand(bot, sync_commands=True)
 
 
-@bot.command(name='commands')
-async def _commands(ctx, *args):
-    try:
-        await ctx.send_help(*args)
-    except Exception as e:
-        print(f"An error occurred while sending help: {e}")
+# @bot.command(name='commands')
+# async def _commands(ctx, *args):
+#     try:
+#         await ctx.send_help(*args)
+#     except Exception as e:
+#         logging.error(f"An error occurred while sending help: {e}")
 
 
 ## Run scripts from folders
@@ -81,14 +84,14 @@ for folder in folders:
         if filename.endswith('.py'):
             script_name = filename[:-3]
             if script_name in IGNORE_SCRIPTS:
-                print(f'Ignored extension: {folder}.{script_name}')
+                logging.info(f'Ignored extension: {folder}.{script_name}')
                 continue
             try:
                 extension = f'{folder}.{script_name}'
                 bot.load_extension(extension)
-                print(f'Loaded extension: {extension}')
+                logging.info(f'Loaded extension: {extension}')
             except Exception as e:
-                print(f'Failed to load extension {extension}: {str(e)}')
+                logging.error(f'Failed to load extension {extension}: {str(e)}')
 
 
 ## Events
@@ -115,9 +118,13 @@ async def on_ready():
     print(f'Number of slash commands: {len(slash.commands)}')
     print('Slash commands:')
     for i, (command_name, command) in enumerate(slash.commands.items(), start=1):
-        print(f'{i}. {command_name}: {command.description}')
+        print(f'{i}. {command_name}')
     await bot.change_presence(status=discord.Status.online, activity=discord.Streaming(name=STREAM_NAME, url=STREAM_URL))
 
+@bot.event
+async def on_disconnect():
+    print("Bot has disconnected.")
+    await slash.remove_all_commands(guild_id=GUILDID)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -139,7 +146,7 @@ async def on_command_error(ctx, error):
     else:
         tb_lines = traceback.format_exception(type(error), error, error.__traceback__)
         tb_text = ''.join(tb_lines)
-        print(f'Error: {error}\n{tb_text}')
+        logging.error(f'Error: {error}\n{tb_text}')
         await ctx.send("An unexpected error occurred. Please try again later.")
 
 
@@ -148,8 +155,12 @@ if __name__ == "__main__":
     try:
         bot.run(TOKEN)
     except discord.LoginFailure:
-        print("Error: Invalid bot token. Check your token configuration.")
+        logging.error("Error: Invalid bot token. Check your token configuration.")
     except discord.HTTPException as e:
-        print(f"Error: Discord HTTP error - {e}")
+        logging.error(f"Error: Discord HTTP error - {e}")
+    except discord.PrivilegedIntentsRequired:
+        logging.error("Error: The bot is missing one or more of the privileged intents.")
+    except discord.ConnectionClosed as e:
+        logging.error(f"Error: The gateway connection is closed. Reason: {e}")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        logging.error(f"An unexpected error occurred: {e}")
